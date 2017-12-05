db128e951e127d078d9f39693e23528ee0b1c566
Khai Bui <khaivubui@gmail.com>
2017-12-04 22:45:08 -0800

    Make duplicates of full templates and clean up templates

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_full.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_full.py
@@ -8,14 +8,24 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+# Taking care of missing data
+# from sklearn.preprocessing import Imputer
+# X[:, 1:3] = Imputer().fit(X[:, 1:3]).transform(X[:, 1:3])
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+X[:, 0] = LabelEncoder().fit_transform(X[:, 0])
+X = OneHotEncoder(categorical_features = [0]).fit_transform(X).toarray()
+
+y = LabelEncoder().fit_transform(y)
+
 # Splitting dataset to Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature scaling
-"""
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
-"""
